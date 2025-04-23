665c6bacb9a03526da20f6316edf24a6e3fd9764
chohne <christopher.hohne@gmail.com>
2025-04-23 17:26:58 -0400

    Multiple orbit targets
    Updated planet class to support multiple orbit targets by using a vector of CelestialBodies

--- planet.cpp
+++ planet.cpp
@@ -6,7 +6,7 @@
 const double G = 6.6743e-11;
 
 // Constructor function for planet
-Planet::Planet(string name, Vector2 position, Vector2 momentum, CelestialBody orbitTarget, int mass, int radius, Color color, string info) {
+Planet::Planet(string name, Vector2 position, Vector2 momentum, std::vector<CelestialBody> orbitTargets, int mass, int radius, Color color, string info) {
 	this->name = name;
 	this->position = position;
 	this->mass = mass;
@@ -14,16 +14,23 @@ Planet::Planet(string name, Vector2 position, Vector2 momentum, CelestialBody or
 	this->color = color;
 	this->info = info;
 
-	this->orbitTarget = orbitTarget;
+	this->orbitTargets = orbitTargets;
 	this->momentum = momentum;
 }
 
-// Calculates the current force on the planet and updates the momentum accordingly
+// Calculates the current net force on the planet and updates the momentum accordingly
 void Planet::updateMomentum(double deltaTime) {
-	double magnitude = -G * (mass * orbitTarget.mass) / pow(Vector2Distance(orbitTarget.position, position), 2);
-	Vector2 direction = Vector2Normalize(orbitTarget.position - position);
 
-	momentum += Vector2Scale(direction, magnitude * deltaTime);
+	// Iterate over each object this planet is orbiting and calculate the gravitational force
+	Vector2 netForce = Vector2Zero();
+	for (const CelestialBody& orbitTarget : orbitTargets) {
+		double magnitude = -G * (mass * orbitTarget.mass) / pow(Vector2Distance(orbitTarget.position, position), 2);
+		Vector2 direction = Vector2Normalize(orbitTarget.position - position);
+		netForce += Vector2Scale(direction, magnitude);
+	}
+
+	// Update momentum based on the net force and timestep
+	momentum += Vector2Scale(netForce, deltaTime);
 }
 
 // Updates the position of the planet based on its current momentum
--- planet.h
+++ planet.h
@@ -2,24 +2,25 @@
 
 #pragma once
 #include "celestialBody.h"
+#include <vector>
 
 class Planet : public CelestialBody {
     // Define a private attributes for velocity and the body that the planet is orbiting.
 private:
     Vector2 momentum;
-    CelestialBody orbitTarget;
-
+    std::vector<CelestialBody> orbitTargets;
+    
     // Define public methods to calculate the force acting on and the changes in position and velocity of a given planet. The
     // parameters of these methods will need to be added.
 public:
     // Creates a new planet
-    // (string: name, Vector2: position, Vector2: momentum, CelestialBody: orbitTarget, int: mass, int: radius, Color: color, string: info)
-    Planet(string, Vector2, Vector2, CelestialBody, int, int, Color, string);
+    // (string: name, Vector2: position, Vector2: momentum, CelestialBody: orbitTargets, int: mass, int: radius, Color: color, string: info)
+    Planet(string, Vector2, Vector2, std::vector<CelestialBody>, int, int, Color, string);
 
-    // Calculates the current force on the planet and updates the momentum accordingly
+    // Calculates the current net force on the planet and updates the momentum accordingly
     // (double: deltaTime)
     void updateMomentum(double);
-
+    
     // Updates the position of the planet based on its current momentum
     // (double: deltaTime)
     void updatePosition(double);
